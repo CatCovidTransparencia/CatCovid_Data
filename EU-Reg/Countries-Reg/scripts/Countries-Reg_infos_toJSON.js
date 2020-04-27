4e4c85ea5308db04c734721896c4c576ab4f1085
CatCovidTransparencia <catcovidtransparencia@gmail.com>
2020-04-27 20:07:12 +0200

    new

--- EU-Reg/Countries-Reg/scripts/Countries-Reg_infos_toJSON.js
+++ EU-Reg/Countries-Reg/scripts/Countries-Reg_infos_toJSON.js
@@ -2,26 +2,35 @@
 
 const fs = require("fs");
 
-var expr_linebreak = /\r\n|\r|\n/;
+let C_attr = "EU";
+// C_attr = "ES";
+// C_attr = "IT";
+// C_attr = "FR";
 
-let r_file = "EU_infos.csv";
+let r_file = C_attr + "_infos.csv";
 let r_path = "../";
 let r_pathfile = r_path + r_file;
 console.log("r_pathfile : " + r_pathfile);
 
-let w_file = "EU_infos.json";
-let w_path = "../../../../../../CatCovid_data/EU-Reg/";
+let w_file = C_attr + "_infos.json";
+let w_path = "../";
 let w_pathfile = w_path + w_file;
 console.log("w_pathfile : " + w_pathfile);
 
+let w_file_2 = C_attr + "_infos_ISO.json";
+let w_path_2 = "../";
+let w_pathfile_2 = w_path_2 + w_file_2;
+console.log("w_pathfile_2 : " + w_pathfile_2);
+
 let raw_data = fs.readFileSync(r_pathfile);
-let lines = raw_data.toString().split(expr_linebreak);
+let lines = raw_data.toString().split("\n");
 
 let headers = [];
 let headers_arr = lines[0].split(",");
 console.log("headers_arr : " + headers_arr);
 
 let file_obj = {};
+let file_obj_2 = {};
 // file_obj["attr"] = "ert";
 
 for (let i = 1; i < lines.length; i++) {
@@ -30,6 +39,7 @@ for (let i = 1; i < lines.length; i++) {
   let name = line_arr[1];
   let pop = line_arr[2];
   let km2 = line_arr[3];
+  let code = line_arr[4];
   console.log(name);
   let line_obj = {};
   for (let j = 0; j < line_arr.length; j++) {
@@ -43,10 +53,13 @@ for (let i = 1; i < lines.length; i++) {
     }
   }
   file_obj[name] = line_obj;
+  file_obj_2[ISO] = line_obj;
 }
 
 var jsonContent = JSON.stringify(file_obj, null, 2);
-
 fs.writeFileSync(w_pathfile, jsonContent);
+
+var jsonContent_2 = JSON.stringify(file_obj_2, null, 2);
+fs.writeFileSync(w_pathfile_2, jsonContent_2);
 
 console.log("_end");
